d04307d60fb153c84dd133d1e839024f18b86e62
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-22 00:47:31 +0200

    Corrected association from user to food

--- db/migrate/20231219195343_create_recipe_foods.rb
+++ db/migrate/20231219195343_create_recipe_foods.rb
@@ -1,8 +1,8 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.1]
   def change
     create_table :recipe_foods do |t|
-      t.references :user, foreign_key: true, null: false
       t.references :recipe, foreign_key: true, null: false
+      t.references :food, foreign_key: true, null: false
       t.integer :quantity
       t.timestamps
     end
