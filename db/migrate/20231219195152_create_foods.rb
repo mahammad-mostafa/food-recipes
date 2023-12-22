5fd571e87f19b40711dc3b0db0b18c9be64389b8
Mahammad Mostafa <mahammad.mostafa@hotmail.com>
2023-12-22 19:38:52 +0200

    Added quantity numerical column

--- db/migrate/20231219195152_create_foods.rb
+++ db/migrate/20231219195152_create_foods.rb
@@ -5,6 +5,7 @@ class CreateFoods < ActiveRecord::Migration[7.1]
       t.string :name
       t.string :measurement_unit
       t.float :price, precision: 10, scale: 2
+      t.integer :quantity
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_19_201456) do
     t.string "name"
     t.string "measurement_unit"
     t.float "price"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
